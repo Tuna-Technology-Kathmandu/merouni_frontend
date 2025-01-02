d43ad9f57844eee5bd6571d9a3e56f67aa44b906
Lucifer203 <adarshghimire10@gmail.com>
2025-01-02 15:18:16 +0545

    Navbar

---
+++ my-app/src/app/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="flex bg-gray-100 p-3 border-b-2 md:px-8 sm-no-x-padding">
+      This is the Footer Section
+    </div>
+  );
+};
+
+export default Footer;
