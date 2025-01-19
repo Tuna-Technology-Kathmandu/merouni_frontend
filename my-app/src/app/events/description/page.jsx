f30bd258d7ad8e1086666ded066b3cdad6606416
Suyog Acharya <asuyog042@gmail.com>
2025-01-19 15:11:20 +0545

    events description page added

---
+++ my-app/src/app/events/description/page.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Navbar from "../../components/Frontpage/Navbar";
+import Footer from "../../components/Frontpage/Footer";
+import Header from "../../components/Frontpage/Header";
+import Hero from "./components/Hero";
+import Description from "./components/Description";
+import Cardlist from "./components/Cardlist";
+const page = () => {
+  return (
+    <div>
+      <Header />
+      <Navbar />
+      <Hero />
+      <Description />
+      <Cardlist />
+      <Footer />
+    </div>
+  );
+};
+
+export default page;
