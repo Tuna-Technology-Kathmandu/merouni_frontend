db5f89e22a39664fbe8bd9504928bcdebf4ab69c
AbishekKhanal <abishekkhanal2056@gmail.com>
2025-01-29 20:23:30 +0545

    Add media management features with upload, fetch, and delete functionality

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
+    mediaUrl: "https://uploads.merouni.com",
     baseUrl: "https://backend.merouni.com",
     version: "/api/v1",
   },
