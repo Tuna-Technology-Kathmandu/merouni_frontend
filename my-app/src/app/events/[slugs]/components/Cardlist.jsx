c19b2f3f38c5fb61d7d3d084be246e48b88697f3
AbishekKhanal <abishekkhanal2056@gmail.com>
2025-02-06 16:05:47 +0545

    asda

--- my-app/src/app/events/[slugs]/components/Cardlist.jsx
+++ my-app/src/app/events/[slugs]/components/Cardlist.jsx
@@ -1,5 +1,3 @@
-
-
 import React from "react";
 import EventCard from "./Cards"; // Import the EventCard component
 
@@ -11,15 +9,16 @@ const Cardlist = ({ events }) => {
           Other events you may like
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          {console.log(events)}
           {events.map((event) => (
             <EventCard
-              key={event._id}
-              photo="/images/eventsdesc.png"
-              month={new Date(event.eventMeta.startDate).toLocaleString('default', { month: 'long' })}
-              day={new Date(event.eventMeta.startDate).getDate()}
-              title={event.title}
-              description={event.description}
-            />
+            key={event.id}
+            photo="/images/eventsdesc.png"
+            month={new Date(event.start_date).toLocaleString('default', { month: 'long' })}
+            day={new Date(event.start_date).getDate()}
+            title={event.title}
+            description={event.description}
+          />
           ))}
         </div>
       </div>
