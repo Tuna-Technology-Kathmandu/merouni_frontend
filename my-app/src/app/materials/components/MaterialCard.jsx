1e86a586feae21348e1ea6aac637b687aa10ad45
AbishekKhanal <abishekkhanal2056@gmail.com>
2025-02-11 13:47:28 +0545

    Add loading state and shimmer effect for featured colleges in Hero component

--- my-app/src/app/materials/components/MaterialCard.jsx
+++ my-app/src/app/materials/components/MaterialCard.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import { Search } from "lucide-react";
 import { getMaterials } from "../action";
 import Pagination from "../../blogs/components/Pagination";
-import Loading from "../../components/Loading";
+import Shimmer from "../../components/Shimmer";
 import Link from "next/link";
 
 const MaterialCard = () => {
@@ -81,14 +81,38 @@ const MaterialCard = () => {
               type="text"
               placeholder="Search material..."
               className="w-full p-2 pl-10 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-             // onChange={(e) => setSearchTerm(e.target.value)}
+              // onChange={(e) => setSearchTerm(e.target.value)}
             />
             <Search className="absolute left-3 top-2.5 h-5 w-5 text-gray-400" />
           </div>
         </div>
 
         {loading ? (
-          <Loading />
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {Array(6)
+              .fill("")
+              .map((_, index) => (
+                <div
+                  key={index}
+                  className="bg-white rounded-xl p-6 border border-gray-200 shadow-lg"
+                >
+                  <div className="flex justify-evenly items-start mb-4">
+                    <div className="w-12 h-12 bg-gray-200 rounded-full flex items-center justify-center">
+                      <Shimmer width="30px" height="30px" />
+                    </div>
+                    <div className="flex flex-col gap-4 w-full">
+                      <Shimmer width="80%" height="20px" />
+                      <Shimmer width="60%" height="18px" />
+                      <Shimmer width="90%" height="15px" />
+                      <div className="flex gap-2">
+                        <Shimmer width="40%" height="15px" />
+                        <Shimmer width="40%" height="15px" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+          </div>
         ) : (
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 p-4">
             {blogs.map((blog, index) => (
