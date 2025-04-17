790ec3c26670e4816aa192960cbc1598bfbbbd06
Milan Rai <milanraiyt07@gmail.com>
2025-04-17 15:50:51 +0545

    added banner gif, spacing in homepage

--- my-app/src/app/components/Frontpage/BannerLayout.jsx
+++ my-app/src/app/components/Frontpage/BannerLayout.jsx
@@ -79,15 +79,13 @@ const BannerLayout = () => {
 
   return (
     // <div className="grid grid-cols-1 w-full sm-d:grid-cols-3 lg:grid-cols-3 gap-7 sm:gap-4 mb-5">
-    <div className="grid grid-cols-2 gap-4 sm:flex md:gap-3 lg:gap-3 justify-center sm:flex-nowrap xl:flex-nowrap">
+    <div className=" flex flex-col sm:flex-row gap-4 md:gap-3 lg:gap-3 justify-center sm:flex-nowrap xl:flex-nowrap">
       {banners.map((item, index) => (
         <div key={item.id}
-          className={`sm:w-[350px] lg:w-[340px] xl:w-full cursor-pointer
-           ${index === 2 ? 'w-[calc(200%+16px)] sm:w-[350px]' : 'w-full'}
-          `}>
+          className={`w-full sm:w-[350px] lg:w-[340px] xl:w-full cursor-pointer bg-slate-400 rounded-lg `}>
           <a href={item.website_url} target="_blank" rel="noopener noreferrer">
             <img
-              src={item.banner_galleries?.[0]?.url || "/images/exampleBanner.jpg"}
+              src={item.banner_galleries?.[0]?.url || "/images/meroUniLarge.gif"}
               onError={(e) => {
                 e.target.src = "/images/exampleBanner.jpg";
               }}
