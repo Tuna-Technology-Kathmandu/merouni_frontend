7279832f73d1df8c1f253f5a7a4482abd62a6cdb
Milan rai <milanraiyt07@gmail.com>
2025-07-09 15:50:59 +0545

    content table fix try

--- src/app/colleges/[slugs]/components/sections/OverviewSection.jsx
+++ src/app/colleges/[slugs]/components/sections/OverviewSection.jsx
@@ -8,54 +8,59 @@ const OverviewSection = ({ college }) => {
       <p className='text-gray-800 mt-9 max-[1120px]:mt-5 leading-7 max-md:leading-5 text-xs md:text-sm lg:text-base text-justify'>
         {college?.description}
       </p>
-      <div
-        dangerouslySetInnerHTML={{ __html: he.decode(college?.content) }}
-        className='text-gray-800 mt-4 leading-7   text-justify 
-             [&>iframe]:w-full 
-             [&>iframe]:max-w-[calc(100vw-40px)] 
-             [&>iframe]:aspect-video 
-             [&>iframe]:h-auto
-             [&>iframe]:rounded-lg 
-             [&>iframe]:mt-4
-             [&>iframe]:mx-auto
-             [&>iframe]:block
 
-              [&_table]:w-full
-    [&_table]:my-4
-    [&_table]:border-collapse
-    [&_th]:bg-gray-100
-    [&_th]:p-2
-    [&_th]:text-left
-    [&_th]:border
-    [&_th]:border-gray-300
-    [&_td]:p-2
-    [&_td]:border
-    [&_td]:border-gray-300
-    [&_tr:nth-child(even)]:bg-gray-50
+      {/* Scrollable container for tables on small screens */}
+      <div className='overflow-x-auto'>
+        <div
+          dangerouslySetInnerHTML={{ __html: he.decode(college?.content) }}
+          className='text-gray-800 mt-4 leading-7 text-justify 
+            [&>iframe]:w-full 
+            [&>iframe]:max-w-[calc(100vw-40px)] 
+            [&>iframe]:aspect-video 
+            [&>iframe]:h-auto
+            [&>iframe]:rounded-lg 
+            [&>iframe]:mt-4
+            [&>iframe]:mx-auto
+            [&>iframe]:block
 
-    [&_h1]:text-2xl
-    [&_h1]:font-bold
-    [&_h1]:mt-8
-    [&_h1]:mb-4
-    [&_h2]:text-xl
-    [&_h2]:font-bold
-    [&_h2]:mt-6
-    [&_h2]:mb-3
-             text-xs md:text-sm lg:text-base
-             overflow-x-hidden
-              [&_ol]:pl-8 
-  [&_ol]:my-4
-  [&_ol]:space-y-2
-  [&_ul]:list-disc 
-  [&_ul]:pl-8 
-  [&_ul]:my-4
-  [&_ul]:space-y-2
-  [&_li]:pl-2
-  max-lg:[&_ol]:text-sm
-  max-lg:[&_ul]:text-sm
-  max-lg:[&_ol]:space-y-1
-  max-lg:[&_ul]:space-y-1'
-      ></div>
+            [&_table]:w-full
+            [&_table]:my-4
+            [&_table]:border-collapse
+            [&_th]:bg-gray-100
+            [&_th]:p-2
+            [&_th]:text-left
+            [&_th]:border
+            [&_th]:border-gray-300
+            [&_td]:p-2
+            [&_td]:border
+            [&_td]:border-gray-300
+            [&_tr:nth-child(even)]:bg-gray-50
+
+            [&_h1]:text-2xl
+            [&_h1]:font-bold
+            [&_h1]:mt-8
+            [&_h1]:mb-4
+            [&_h2]:text-xl
+            [&_h2]:font-bold
+            [&_h2]:mt-6
+            [&_h2]:mb-3
+
+            text-xs md:text-sm lg:text-base
+            overflow-x-hidden
+            [&_ol]:pl-8 
+            [&_ol]:my-4
+            [&_ol]:space-y-2
+            [&_ul]:list-disc 
+            [&_ul]:pl-8 
+            [&_ul]:my-4
+            [&_ul]:space-y-2
+            [&_li]:pl-2
+            max-lg:[&_ol]:text-sm
+            max-lg:[&_ul]:text-sm
+            max-lg:[&_ol]:space-y-1
+            max-lg:[&_ul]:space-y-1'
+        />
+      </div>
 
       <h2 className='text-[13px] md:text-[15px] lg:text-[17px] font-bold mt-6'>
         Institution Type
