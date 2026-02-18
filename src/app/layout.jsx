e6dde84faf1d9816cd05f59e2a5352296dc78bab
Saroj Dahal <isarojdahal@gmail.com>
2026-02-18 15:42:45 +0545

    setup: google analytics

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,5 @@
 import { Poppins } from 'next/font/google'
+import Script from 'next/script'
 import './globals.css'
 import 'react-toastify/dist/ReactToastify.css'
 import { ToastContainer } from 'react-toastify'
@@ -36,6 +37,19 @@ export default function RootLayout({ children }) {
             />
           </ReactQueryContainer>
         </ReduxProvider>
+        <Script
+          src='https://www.googletagmanager.com/gtag/js?id=G-C5JXZHZZC2'
+          strategy='afterInteractive'
+        />
+        <Script id='google-analytics' strategy='afterInteractive'>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-C5JXZHZZC2');
+          `}
+        </Script>
       </body>
     </html>
   )
