fe5f42b7bca10334566a60168bbd3f7b8622f2f6
Sujan Budhathoki <iamsujanbudhathoki@gmail.com>
2026-02-24 13:18:14 +0545

    remove that scrolsidebar

--- src/ui/molecules/dialogs/college/CreateUpdateCollegeModal.jsx
+++ src/ui/molecules/dialogs/college/CreateUpdateCollegeModal.jsx
@@ -471,7 +471,7 @@ const CreateUpdateCollegeModal = ({
                             </div>
                         </div>
                     )}
-                    <div className='flex-1 overflow-y-auto p-8 scrollbar-thin scrollbar-thumb-gray-200'>
+                    <div className='flex-1'>
                         <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 pb-8">
                             {/* Left Column - Main Content (8/12) */}
                             <div className="lg:col-span-8 space-y-8">
